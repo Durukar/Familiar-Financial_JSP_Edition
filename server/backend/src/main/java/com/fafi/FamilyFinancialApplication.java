c7d9d1e26535e205c79e981932e5afac2ad1b42d
Durukar <nightdavila@gmail.com>
2025-03-13 23:34:09 -0300

    Initial configuration, ws, exceptions and others.

---
+++ server/backend/src/main/java/com/fafi/FamilyFinancialApplication.java
@@ -0,0 +1,13 @@
+package com.fafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFinancialApplication.class, args);
+	}
+
+}
---
+++ server/backend/src/main/java/com/fafi/controllers/GreetingsController.java
@@ -0,0 +1,18 @@
+package com.fafi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingsController {
+
+
+    @GetMapping()
+    public String greetings() {
+        return "Greetings from Spring Boot!";
+    }
+
+
+}
